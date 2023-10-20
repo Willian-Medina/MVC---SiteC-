5c95ce2459600677246f5a7420b34abcd9066d25
Willian Medina <willian-pablomedina@hotmail.com>
2023-10-19 22:30:20 -0300

    terminado o model dos livros

---
+++ WebApplication1/Models/EmprestimoModel.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApplication1.Models
+{
+    //reprentará uma coluna no BD
+    public class EmprestimoModel
+    {
+     //Campos da tablea BD
+
+     public int Id { get; set; }
+     public string Recebedor { get; set; }
+     public string Fornecedor { get; set; }
+     
+     public string LivroEmprestado { get; set; }
+     public DateTime DataEmprestimo { get; set; } = DateTime.Now;
+
+
+
+ 
+
+
+    }
+}
